0d290699a73636dd046c33f7d80bc6f2a3513e13
Jack <jack@Jacks-MacBook-Air.local>
2019-04-04 18:53:11 -0400

    wassup bois

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://fdo14:Terrysducks1!@ds237389.mlab.com:37389/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
